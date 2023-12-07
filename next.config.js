ff04203ed9ed8a7c20a8e739663dfc0edf153e0d
lucky <lrathore@tryspare.com>
2023-12-07 17:31:48 +0530

    message

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'http://localhost:5215/api/:path*',
-          },
-        ]
+  trailingSlash: true,
+  output: 'export',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5215/api/:path*',
       },
-  };
+    ]
+  },
+};
